01e4bd1f789afb733c9e1e1baeeb947e599cffae
Lucas Motta <lucasriann7890@gmail.com>
2024-08-31 19:59:46 -0300

    Add files via upload

---
+++ 31-08/exerciciosWhile/src/exerciciosWhile/Exercicio3.java
@@ -0,0 +1,18 @@
+package exerciciosWhile;
+public class Exercicio3 {
+	public static void main(String[] args) {
+		
+		int i = 0;
+		int n = 0;
+		
+		while(i<101) {
+		
+			System.out.println(n);
+			
+		i =  i + 1;
+		n = i + n;
+		
+		
+	}
+  }
+}
