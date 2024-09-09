041636764f165cbb83150531a741b4113529ddfa
Lucas Motta <lucasriann7890@gmail.com>
2024-09-09 17:18:04 -0300

    Update ExercicioE.java

--- 0209/exercicios0209/src/exercicios0209/ExercicioE.java
+++ 0209/exercicios0209/src/exercicios0209/ExercicioE.java
@@ -4,28 +4,26 @@ public class ExercicioF {
 	public static void main(String[] args) {
 		
 		Scanner ler = new Scanner(System.in);
-			int n, divisor, resto;
+			int n, div, resto;
 		
 		System.out.println("Digite o número que deseja-se saber se é primo:");
 			n = ler.nextInt();
-	
-	while(div<n) {
+			div = n;
+	while(div>0) {
 
-			
-			
 	     if(n%div == 0){
-			resto++;
-				}
-			div++;
+			resto = resto + 1;
+}
+				div--;
 }
 		
-	     if(resto == 2) {
+	     if(resto==2) {
 		
-		System.out.println("é primo");
+		System.out.println("O número "+n+" é primo.");
 		
 }
 	     else {
-		System.out.println("não é");
+		System.out.println("O número "+n+" não é primo.");
 }
 ler.close();
 }
