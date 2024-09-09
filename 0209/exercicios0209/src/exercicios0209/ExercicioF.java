f10e46cbc69eca70954f312b29fc81a38df91a62
Lucas Motta <lucasriann7890@gmail.com>
2024-09-09 12:14:05 -0300

    Update ExercicioF.java

--- 0209/exercicios0209/src/exercicios0209/ExercicioF.java
+++ 0209/exercicios0209/src/exercicios0209/ExercicioF.java
@@ -5,16 +5,38 @@ public class ExercicioF {
 		
 		Scanner ler = new Scanner(System.in);
 		
-		    int n, i = 0;
+			int n, res1 = 0, res2 = 0, res3 = 0;
+			
+			int div;
 		
-		System.out.println("Digite o número que deseja saber se é ímpar:");
-		    n = ler.nextInt();
-		    
-		    
-		    
-		    
-		    
-		    
-	}
-
+		
+				
+		System.out.println("Digite o número que deseja-se saber se é primo:");
+			n = ler.nextInt();
+				
+			div = n;
+	
+		while(div>1) {
+		
+	
+			res1 =	(n%1) +	(n%n);
+			
+			res2 =  (n%2);	
+		
+			res3 = (n%3);
+				
+			div--;
+		
+}//fim while
+		
+	if(res1==0 && res2>0 && res3>0) {
+		
+			System.out.println("é primo");
+		
+}
+	else {
+			System.out.println("não é");
+}
+		ler.close();
+}
 }
